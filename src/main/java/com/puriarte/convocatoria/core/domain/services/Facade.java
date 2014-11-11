db4c1deae29084495b9d1e3b34ff6d2e4e014d0f
Pablo <Pablo@Nati>
2014-11-10 23:52:50 -0200

    -

--- src/main/java/com/puriarte/convocatoria/core/domain/services/Facade.java
+++ src/main/java/com/puriarte/convocatoria/core/domain/services/Facade.java
@@ -1,6 +1,9 @@
 package com.puriarte.convocatoria.core.domain.services;
 
 import com.puriarte.convocatoria.persistence.AssignmentStatus;
+import com.puriarte.convocatoria.persistence.Movil;
+import com.puriarte.convocatoria.persistence.MovilStatus;
+import com.puriarte.convocatoria.persistence.Person;
 import com.puriarte.convocatoria.persistence.PersonCategory;
 import com.puriarte.convocatoria.persistence.PersonMovil;
 
@@ -30,6 +33,36 @@ public class Facade {
 	}
 
 	public PersonCategory selectPersonCategory(int categoryId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public MovilStatus selectMovilStatus(int movilStatusPending) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int insertPerson(Person person) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void insertMovil(Movil movil) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void insertPersonMovil(PersonMovil pm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Person getPerson(String document, int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object selectMovil(String movilNumber, int movilStatusActive) {
 		// TODO Auto-generated method stub
 		return null;
 	}
